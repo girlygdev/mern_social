7431fcb0f69e1fe51d90a058d316b56dc2ab9f2b
Gil <girlyg@goetu.com>
2021-09-01 10:41:41 +0800

    Add user schema and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const userValidator = [
+  check('name', 'Name is required.').not().isEmpty(),
+  check('email', 'Please include a valid email.').isEmail(),
+  check('password', 'Password should at least be 6 characters.').isLength({ min: 6 })
+];
 
 /**
  * @route  GET api/users
- * @desc   Test route
+ * @desc   Register user
  * @access Public
  */
-router.get('/', (req, res) => {
+router.post('/', userValidator, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors : errors.array() });
+  }
+
   res.send('User route')
 })
 
